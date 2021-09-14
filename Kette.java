ef4ea4cbb60edb07b361b147ffa584b861d3a6c9
Jörn Schrader <88908735+JoernWVSG@users.noreply.github.com>
2021-09-14 14:51:06 +0200

    add Kette.java

---
+++ Kette.java
@@ -0,0 +1,96 @@
+public class Kette {
+
+    private int anzahl;                   // Anzahl der Lampen in der Kette
+    private Lampe[] kette;                // Feld mit den Lampen
+    private String farbeNaechsteLampe;    // Farbe der naechsten Lampe
+
+    /**
+     *   Konstruktor initialisiert Feld für 10 Lampen,
+     *   setzt die Farbe der ersten Lampe auf rot.
+     */
+    public Kette() {
+        this.kette = new Lampe[10];        
+        this.anzahl = 0;
+        this.farbeNaechsteLampe = "rot";        
+    }
+
+    /**
+     *   Fuegt wenn moeglich neue Lampe ans Ende der Kette.
+     *   Aendert Farbe der naechsten Lampe.
+     */
+    public void hintenEinfuegen() {
+        if(this.anzahl < 10) {
+            /**
+             *  Neue Lampe nur dann, wenn noch keine 10 Lampen vorhanden sind.
+             */
+            this.kette[anzahl] = new Lampe(10 + this.anzahl * 10, 100, farbeNaechsteLampe);
+            this.anzahl = this.anzahl + 1;
+            /**
+             *  Farbwechsel
+             */
+            if (this.farbeNaechsteLampe == "rot") {
+                this.farbeNaechsteLampe = "weiss";
+            } else {
+                this.farbeNaechsteLampe = "rot";
+            }
+        }
+    }
+
+    /**
+     *  Entfernt wenn moeglich Lampe vom Beginn der Kette.
+     */
+    public void vorneEntfernen() {
+        if(this.anzahl > 0) {
+            int i;
+            /**
+             *  Lampe muss ausgeschaltet werden
+             */
+            this.kette[0].setzeSichtbar(false);
+            /**
+             *  Jetzt restlichen Lampen nach vorn rücken, 
+             *  visuell und im Feld.
+             */
+            for(i=0; i<this.anzahl-1; i=i+1) {
+                this.kette[i] = this.kette[i+1];
+                this.kette[i].setzeMittelpunkt(10+i*10,100);
+            }
+            this.kette[i] = null;
+            this.anzahl = this.anzahl - 1;
+        }
+    }
+
+    /**
+     *  Entfernt Lampe an angegebener Position
+     *  
+     *  @param position Index der Lampe
+     */
+    public void entfernePosition(int position) {
+        if (position < this.anzahl) {
+            int i;
+            this.kette[position].setzeSichtbar(false);
+            for(i=position; i<this.anzahl-1; i=i+1) {
+                this.kette[i] = this.kette[i+1];
+                this.kette[i].setzeMittelpunkt(10+i*10,100);
+            }
+            this.kette[i] = null;
+            this.anzahl = this.anzahl - 1;
+        }
+    }
+
+    /**
+     *  Entfernt Lampen der angegebene  Farbe
+     *  
+     *  @param farbe Lampenfarbe
+     */
+    public void entferneFarbe(String farbe) {
+        if (this.anzahl > 0) {
+            for (int i=0; i<this.anzahl; i=i+1) {
+                if (this.kette[i].nenneFarbe() == farbe) {
+                    this.entfernePosition(i);
+                    i = i - 1;
+                }
+            }            
+        }
+    }
+
+}
